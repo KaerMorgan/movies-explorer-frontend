c7bebc75dcc55f5ec360c822b0362b2d2421eafc
Danil Filatov <Subdante@gmail.com>
2022-10-09 00:24:17 +0300

    Task #2 landing page completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='footer'>
+      <p className='footer__hint'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <hr className='footer__line-break' />
+      <div className='footer__text-group'>
+        <p className='footer__copyright'>© {new Date().getFullYear()}</p>
+
+        <nav className='footer__links'>
+          <a href='' className='footer__link'>
+            Яндекс.Практикум
+          </a>
+          <a href='' className='footer__link'>
+            Github
+          </a>
+        </nav>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from '../Header';
 import globe from '../../images/globe.png';
+import me from '../../images/main__photo.jpg';
 import './Main.scss';
 
 const Main = () => {
-  console.log(window.screen.width);
   return (
     <>
       <Header />
@@ -27,7 +27,6 @@ const Main = () => {
 
         <section className='main__section main__about'>
           <h2 className='main__section-title'>О проекте</h2>
-
           <hr className='main__line-break' />
 
           <div className='main__about-text-group'>
@@ -57,6 +56,75 @@ const Main = () => {
               <div className='front-end-caption'>Front-end</div>
             </div>
           </div>
+        </section>
+
+        <section className='main__section main__technologies'>
+          <h2 className='main__section-title'>Технологии</h2>
+          <hr className='main__line-break main__technologies-line-break' />
+          <h3 className='main__technologies-title'>7 технологий</h3>
+          <p className='main__technologies-text'>
+            На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
+          </p>
+          <ul className='main__technologies-list'>
+            <li className='main__technologies-list-item'>HTML</li>
+            <li className='main__technologies-list-item'>CSS</li>
+            <li className='main__technologies-list-item'>JS</li>
+            <li className='main__technologies-list-item'>React</li>
+            <li className='main__technologies-list-item'>Git</li>
+            <li className='main__technologies-list-item'>Express.js</li>
+            <li className='main__technologies-list-item'>mongoDB</li>
+          </ul>
+        </section>
+
+        <section className='main__section main__student'>
+          <h2 className='main__section-title'>Студент</h2>
+          <hr className='main__line-break main__student-line-break' />
+
+          <div className='main__student-text-group'>
+            <h3 className='main__student-title'>Данил</h3>
+            <p className='main__student-subtitle'>Фронт-енд разработчик, 24 года</p>
+            <p className='main__student-text'>
+              Я родом из Крыма. Люблю музыку и кино. Слушаю много подкастов и играю на гитаре.
+              Работаю в компании "Крэлком" фронтэнд-разработчиком. Курс практикума помог внести
+              структуру в моё обучение и заново научил меня писать код, только лучше и осмысленнее.
+            </p>
+            <a href='https://github.com/KaerMorgan' className='main__student-link'>
+              Github
+            </a>
+          </div>
+
+          <img src={me} alt='Данил' className='main__student-photo' />
+
+          <p className='main__student-list-header'>Портфолио</p>
+          <ul className='main__student-list'>
+            <li className='main__student-list-item'>
+              <a
+                href='https://github.com/KaerMorgan/how-to-learn'
+                className='main__student-list-link'
+              >
+                Статичный сайт
+              </a>
+              <p className='main__arrow'>↗</p>
+            </li>
+            <li className='main__student-list-item'>
+              <a
+                href='https://github.com/KaerMorgan/russian-travel'
+                className='main__student-list-link'
+              >
+                Адаптивный сайт
+              </a>
+              <p className='main__arrow'>↗</p>
+            </li>
+            <li className='main__student-list-item'>
+              <a
+                href='https://github.com/KaerMorgan/mesto-react'
+                className='main__student-list-link'
+              >
+                Одностраничное приложение
+              </a>
+              <p className='main__arrow'>↗</p>
+            </li>
+          </ul>
         </section>
       </main>
     </>
